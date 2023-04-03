689cb514760e696188b1fe1cb7c89f92d5e48374
Bohdan Partyk <info@podebrady.org>
2023-04-03 13:29:26 +0200

    terser minify JS

--- webpack/modules/webpack.terser.js
+++ webpack/modules/webpack.terser.js
@@ -4,56 +4,24 @@
  * @type {TerserPlugin}
  */
 const TerserPlugin = require('terser-webpack-plugin');
-const {merge} = require('webpack-merge');
-
+/**
+ * @DOC options https://github.com/terser/terser#minify-options
+ * @param options {{}|undefined}
+ * @param test {string|[]|undefined}
+ * @param include {[]|undefined}
+ * @param exclude {[]|undefined}
+ * @returns {{optimization: {minimize: boolean, minimizer: *[]}}}
+ */
 module.exports = ({
     options,
     test,
     include,
     exclude,
 } = {}) => {
-    /**
-     * @DOC options https://github.com/terser/terser#minify-options
-     * @type {{[p: string]: *}}
-     */
-    options = merge({
-        ...{
-            parse: {
-                // parse options
-            },
-            compress: {
-                // compress options
-            },
-            mangle: {
-                // mangle options
-
-                properties: {
-                    // mangle property options
-                },
-            },
-            format: {
-                // format options (can also use `output` for backwards compatibility)
-            },
-            sourceMap: {
-                // source map options
-            },
-            ecma: 5, // specify one of: 5, 2015, 2016, etc.
-            enclose: false, // or specify true, or "args:values"
-            keep_classnames: false,
-            keep_fnames: false,
-            ie8: false,
-            module: false,
-            nameCache: null, // or specify a name cache object
-            safari10: false,
-            toplevel: false,
-        },
-        ...options,
-    });
     return {
         optimization: {
             minimize: true,
             minimizer: [
-                new TerserPlugin(),
                 new TerserPlugin({
                     include,
                     exclude,
