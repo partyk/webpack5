bb137e39aa5b91cbdd54cf05de9949ad57134139
Bohdan Partyk <info@podebrady.org>
2023-11-04 08:46:23 +0100

    svg-sprite

---
+++ webpack/config/svgo.config.js
@@ -0,0 +1,27 @@
+/**
+ * @DOC https://github.com/svg/svgo
+ */
+module.exports = {
+    multipass: true, // boolean. false by default
+    datauri: 'enc', // 'base64' (default), 'enc' or 'unenc'.
+    js2svg: {
+        indent: 2, // string with spaces or number of spaces. 4 by default
+        pretty: true, // boolean, false by default
+    },
+    plugins: [
+        {
+            name: 'preset-default',
+            params: {
+                overrides: {
+                    // customize default plugin options
+                    inlineStyles: {
+                        onlyMatchedOnce: false,
+                    },
+
+                    // or disable plugins
+                    removeDoctype: false,
+                },
+            },
+        },
+    ],
+};
