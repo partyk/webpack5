f400cebee1025d40b8dbd429949473079657cbff
Bohdan Partyk <info@podebrady.org>
2023-11-14 09:11:29 +0100

    add others setings

--- webpack/config/webpack.common.js
+++ webpack/config/webpack.common.js
@@ -15,13 +15,17 @@ module.exports = merge(
         resolve: {
             extensions: ['.vue', '.js'/*, '.css', '.tsx', '.ts','.json' */],
             modules: [
-                path.resolve(config.path.root, 'node_modules'),
+                path.resolve(__dirname, 'node_modules'),
+                path.resolve(__dirname, 'bower'),
+                path.resolve(__dirname, 'www/vendor'), // bower
             ],
             alias: {
-                '@src': path.resolve(config.path.root, 'src'),
-                '@js': path.resolve(config.path.root, 'src', 'js'),
-                '@scss': path.resolve(config.path.root, 'src', 'scss'),
-                '@images': path.resolve(config.path.root, 'src', 'images'),
+                '@ui': path.resolve(__dirname, 'app/ui'),
+                '@bower': path.resolve(__dirname, 'www/vendor'),
+                '@src': path.resolve(config.path.root, 'app/ui'),
+                '@js': path.resolve(config.path.root, 'app/ui/js'),
+                '@scss': path.resolve(config.path.root, 'app/ui/scss'),
+                '@images': path.resolve(config.path.root, 'app/ui/images'),
                 '@node_modules': path.resolve(config.path.root, 'node_modules'),
             },
         },
@@ -42,16 +46,28 @@ module.exports = merge(
                     '.vue',
                 ],
             }),
-            plugins.htmlWebpack({
+            // aktualne se nevyuziva
+            /* plugins.htmlWebpack({
                 ver: Math.floor(Math.random() * (100000 - 1) + 1),
                 filename: path.resolve(config.path.dist, 'index.html'),
                 template: path.resolve(config.path.src, 'template/index.html'),
                 inject: false,
                 hash: true,
-            }),
+            }), */
         ],
     },
-    loaders.modernizr(),
+    loaders.modernizr({
+        options: {
+            'minify': true,
+            'options': [
+                'setClasses',
+            ],
+            'feature-detects': [
+                'css/flexwrap',
+                'forcetouch',
+            ],
+        },
+    }),
     loaders.vue(),
     loaders.javaScript(),
     loaders.styleCSS({
@@ -94,5 +110,4 @@ module.exports = merge(
         },
     }),
     loaders.imageMinimizer(),
-    // loaders.imageMin(),
 );
